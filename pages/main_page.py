f0b27d88e707775c07cbb850162dfb434a4b732b
zastasja <anastasia_zh@bk.ru>
2022-12-09 18:10:07 +0300

    added sidebar menu tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -20,6 +20,11 @@ class Main_page(Base):
     enter_to_cart = "//div[@class='shopping_cart_container']"
     burger_menu_btn = "//button[@id='react-burger-menu-btn']"
     link_about = "//a[@id='about_sidebar_link']"
+    link_products = "//a[@id='inventory_sidebar_link']"
+    link_logout = "//a[@id='logout_sidebar_link']"
+    link_reset_app = "//a[@id='reset_sidebar_link']"
+
+    shopping_cart_badge = "//span[@class='shopping_cart_badge']"
 
     # Getters
 
@@ -43,6 +48,19 @@ class Main_page(Base):
 
     def get_link_about(self):
         return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_about)))
+
+    def get_link_products(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_products)))
+
+    def get_link_logout(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_logout)))
+
+    def get_link_reset_app(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_reset_app)))
+
+    def get_shop_cart_badge(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.shopping_cart_badge)))
+
 
     #Actions
 
@@ -83,6 +101,22 @@ class Main_page(Base):
         self.get_link_about().click()
         print('click about link')
 
+    def click_products(self):
+        self.get_link_products().click()
+        print('click all products link')
+
+    def click_logout(self):
+        self.get_link_logout().click()
+        print('click logout link')
+
+    def click_reset_app(self):
+        self.get_link_reset_app().click()
+        print('click reset link')
+
+    def text_of_shop_cart_badge(self):
+        word = self.get_shop_cart_badge().text
+        return word
+        print('get shopping cart badge text')
     # Method
     def add_product_1(self):
         self.get_current_url()
@@ -119,3 +153,20 @@ class Main_page(Base):
         self.click_about()
         self.assert_url("https://saucelabs.com/")
 
+    def select_products_sidebar_menu(self):
+        self.get_current_url()
+        self.click_menu()
+        self.click_products()
+        self.assert_url("https://www.saucedemo.com/inventory.html")
+
+    def select_logout_sidebar_menu(self):
+        self.get_current_url()
+        self.click_menu()
+        self.click_logout()
+        self.assert_url("https://www.saucedemo.com/")
+
+    def select_reset_app_sidebar_menu(self):
+        self.get_current_url()
+        self.click_menu()
+        self.click_reset_app()
+        self.assert_url("https://www.saucedemo.com/inventory.html")
---
+++ tests/test_link_reset_app.py
@@ -0,0 +1,14 @@
+import allure
+import pytest
+
+from pages.login_page import Login_page
+from pages.main_page import Main_page
+
+@pytest.mark.parametrize("login_user, login_password",
+                         [('standard_user', 'secret_sauce')])
+@allure.description("test link about")
+def test_link_about(browser, login_user, login_password):
+    page = Login_page(browser)
+    page.authorization(login_user, login_password)
+    mpage = Main_page(browser)
+    mpage.select_reset_app_sidebar_menu()
